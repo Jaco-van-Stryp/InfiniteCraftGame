5749b7983728ac95cb78ff3f4505626a71be3273
Jaco van Stryp <jacovanstryp@gmail.com>
2026-05-02 12:56:50 +1200

    Initial Angular Setup

---
+++ InfiniteCraftClient/src/app/api/api/api.ts
@@ -0,0 +1,4 @@
+export * from './infiniteCraftGame.service';
+import {InfiniteCraftGameService} from './infiniteCraftGame.service';
+
+export const APIS = [InfiniteCraftGameService];
---
+++ InfiniteCraftClient/src/app/api/model/models.ts
@@ -0,0 +1,2 @@
+export * from './combineWordCommand';
+export * from './combineWordResponse';
